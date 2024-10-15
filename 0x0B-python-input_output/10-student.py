cdf87cfbb3c4d372fccad7aa5b4fa77d7941abca
Elfenom <elphenom10@gmail.com>
2024-10-15 13:42:56 -0700

    10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+    Initialize Module
+"""
 
 
-class Student:
-    """Represent a student."""
+class Student():
+    """
+        Defines Student() Class
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+        """
+            Initialization of new Student instance
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+            Retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return (self.__dict__)
+
+        new_dict = {}
+        for i in attrs:
+            if i in self.__dict__.keys():
+                new_dict[i] = self.__dict__[i]
+        return new_dict
