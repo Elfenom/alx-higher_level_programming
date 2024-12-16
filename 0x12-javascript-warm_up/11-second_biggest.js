88b500bbfa6fc72ebff8baf3ad0c5a3ba60af811
Elfenom <elphenom10@gmail.com>
2024-12-16 14:04:35 -0800

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,8 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments
-
-const getSecondMax = (arr) => {
-  let max = parseInt(arr[2]);
-  let secMax = parseInt(arr[2]);
-  
-  if ((arr.length === 2) || (arr.length === 3)) {
-    return (0);
-  }
-    for (let i = 3; i < arr.length; i++) {
-      if (parseInt(arr[i]) >= max) {
-        secMax = max;
-        max = parseInt(arr[i]);
-      } else if (parseInt(arr[i]) > secMax) {
-        secMax = parseInt(arr[i]);
-      }
-    }
-    return (secMax);
-  };
-
-console.log(getSecondMax(process.argv));
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
+}
